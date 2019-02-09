b5833befa003d02745ac1b31d553a49d000fc214
Nathan <nathanj@spu.edu>
2019-02-08 20:09:29 -0800

    Update application-server-v1.py

--- application-server-v1.py
+++ application-server-v1.py
@@ -1,23 +1,32 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import MySQLdb
 
-db = MySQLdb.connect("localhost", "dev", "password", "testDatabase")
-cur= db.cursor()
+db= MySQLdb.connect(host="localhost", user="dev", password="password", db="testDatabase")
+
+cur=db.cursor()
 
 app = Flask(__name__)
 
+#this is a basic example of how flask routes are set up for application server
 @app.route("/")
 def index():
-	return "This is the application server for Tenda."
+    return "This is the application server for Tenda"
 
+#this is an example of retreiving something from the database.
 @app.route("/user/lookup/<id>", methods=['GET'])
 def user(id):
-	cur.execute("Select * from testTable wherer user_id="+id+";")
-	for row in cur.fetchall():
-		testData = row[1]
-	return testData
+    cur.execute("Select * from testTable where user_id="+ id +";")
+    for row in cur.fetchall():
+        testData = row[1]
+    return testData
+
+#this is an example of adding something to the database.
+@app.route("/user/add/<string:email>/<string:password>", methods=['GET', 'POST'])
+def addUser(email, password):
+    sql = "insert into testTable (email, password) values ('" + email + "','"+password + "')"
+    #val = (email, password)
+    cur.execute(sql)
+    db.commit()
+    return jsonify(result={"status": 200})
 
 app.run(host='0.0.0.0', port=80)
-
-if __name__ == "__main__":
-	app.run()
