e48914cf88fbec7cbd2da80a4ad0814b6681d660
Nathan <nathanj@spu.edu>
2019-03-26 13:46:06 -0700

    Update application-server-v1.py

--- application-server-v1.py
+++ application-server-v1.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify
 import MySQLdb
 
-db= MySQLdb.connect(host="localhost", user="dev", password="password", db="testDatabase")
-
+db= MySQLdb.connect(host="localhost", user="root", password="password", db="Tenda")
 cur=db.cursor()
-
 app = Flask(__name__)
 
 #this is a basic example of how flask routes are set up for application server
@@ -12,45 +10,54 @@ app = Flask(__name__)
 def index():
     return "This is the application server for Tenda"
 
-#this is an example of retreiving something from the database.
-@app.route("/user/lookup/<id>", methods=['GET'])
-def user(id):
-    cur.execute("Select * from testTable where user_id="+ id +";")
-    for row in cur.fetchall():
-        testData = row[1]
-    return jsonify(result={testData:id})
 
-#this is an example of adding something to the database.
-@app.route("/user/add/<string:email>/<string:password>", methods=['GET', 'POST'])
-def addUser(email, password):
-    sql = "insert into testTable (email, password) values ('" + email + "','"+password + "')"
+#this is an example of retreiving something from the database.
+@app.route("/user/lookup/<user_id>", methods=['GET'])
+def getUser(user_id):
+    cur.execute("Select firstName, lastName, email from User where user_id="+ user_id +";")
+    firstName, lastName, email = cur.fetchone()
+    return jsonify(result={"id":user_id,"first name":firstName, "last name":lastName, "email":email})
+
+#get event details from an eventID
+@app.route("/event/lookup/<event_id>", methods=['GET'])
+def getEventDetails(event_id):
+    cur.execute("Select manager_id, eventName, eventDate, eventTime, eventDuration, eventRadius, eventDescription from Event where event_id="+ event_id +";")
+    manager_id, eventName, date, time, duration, radius, description = cur.fetchone()
+    return jsonify(result={"managerID":manager_id, "eventName":eventName, "date":str(date), "time":str(time), "duration":str(duration), "radius":str(radius), "description":description})
+
+#complete
+@app.route("/event/attendees/<event_id>/<response>", methods=["GET"])
+def getEventAttendees(event_id, response):
+    cur.execute("Select user_id from Events_to_Attendees where event_id="+ event_id +" AND response='"+response+"';")
+    result = []
+    for row in cur.fetchall():
+        result.append(row)
+    return jsonify(result={"result":result})
+
+#Description: Users and managers can sign up with their email address and set the username and password for the account.
+@app.route("/user/add/<string:firstName>/<string:lastName>/<string:email>/<string:password>", methods=['GET', 'POST'])
+def createUser(firstName, lastName, email, password):
+    sql="insert into User(firstName, lastName, email, password) values('"+firstName+"','"+lastName+"','"+email+"','"+password+"')"
+    cur.execute(sql)
+    db.commit()
+    return jsonify(result={"status": 200})
+
+#Description: A user can become a manager by creating an event. They can then specify the details of the event and send an invitation by the user’s email.
+@app.route("/event/add/<manager_id>/<eventName>/<eventDate>/<eventTime>/<eventDuration>/<eventRadius>/<eventDescription>", methods=['GET', 'POST'])
+def createEvent(manager_id, eventName, eventDate, eventTime, eventDuration, eventRadius, eventDescription):
+    sql = "insert into Event(manager_id, eventName, eventDate, eventTime, eventDuration, eventRadius, eventDescription) values ("+manager_id+",'"+eventName+"','"+eventDate+"','"+eventTime+"','"+eventDuration+"','"+eventRadius+"','"+eventDescription+"');"
+    cur.execute(sql)
+    db.commit()
+    return jsonify(result={"status": 200})
+
+@app.route("/response/add/<event_id>/<user_id>/<response>", methods=['GET', 'POST'])
+def createResponse(event_id, user_id, response):
+    sql = "insert into Events_to_Attendees(event_id, user_id, response) values ("+event_id+","+user_id+",'"+response+"');"
     cur.execute(sql)
     db.commit()
     return jsonify(result={"status": 200})
 
 
-
-#these are the functions we need to create
-#Description: Users and managers can sign up with their email address and set the username and password for the account.
-#Parameters: Email, username, password, firstname, lastname.
-#Verfiy new emails and usernames are unique.
-@app.route("/createAccount/<string:email>/<string:firstname>/<string:lastname>/<string:username>/<string:password>", methods=['GET', 'POST'])
-def createAccount(email, firstname, lastname, username, password):
-    sql = "insert into accountsTable (email, firstname, lastname, username, password) values (%s, %s, %s, %s, %s)"
-    val = (email, firstname, lastname, username, password)
-    cur.execute(sql, val)
-    db.commit()
-    return jsonify(result={"status": 200})
-
-#Description: A user can become a manager by creating an event. They can then specify the details of the event and send an invitation by the user’s email.
-@app.route("/createEvent/", methods=['GET', 'POST'])
-def createEvent():
-    return jsonify(result={"status": 200})
-
-#Description: Users will receive an invitation through email to reply to the event. 
-@app.route("/joinEvent/", methods=['GET', 'POST'])
-def joinEvent():
-    return jsonify(result={"status": 200})
 
 #Description: The manager is able to view the details of the event, the people that opened the link as well as their response. As well as the duration of the user at the event. Recurring events will also have a page for statistical data. 
 @app.route("/manageEvent/", methods=['GET', 'POST'])
