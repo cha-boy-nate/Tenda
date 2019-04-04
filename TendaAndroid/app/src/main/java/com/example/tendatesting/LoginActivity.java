657ee60d7ea29aa3b5ad82303b544e0f4ba9ef00
Hungnguyen <37910067+Hungnguyen15595@users.noreply.github.com>
2019-04-04 14:10:52 -0700

    Android App Commit

---
+++ TendaAndroid/app/src/main/java/com/example/tendatesting/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.tendatesting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+    public void ClickToSignUpPage(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
