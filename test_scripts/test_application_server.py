014ed24db46634774ad897dfd9db3b0611c5bcea
Nathan <nathanj@spu.edu>
2019-04-05 09:01:38 -0700

    Create test_application_server.py

---
+++ test_scripts/test_application_server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/"):
+def home():
+  return jsonify(result={"status":200})
+  
+@app.route("/test"):
+def test():
+  print("\nPrinting Test\n")
+  return jsonify(result={"status":200})
