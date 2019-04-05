37a4a0f20da28594840808fd4da3f4ff6ed178e5
Nathan <nathanj@spu.edu>
2019-04-05 12:00:36 -0700

    Update test_application_server.py

--- test_scripts/test_application_server.py
+++ test_scripts/test_application_server.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route("/"):
+@app.route("/")
 def home():
   return jsonify(result={"status":200})
   
-@app.route("/test"):
+@app.route("/test")
 def test():
   print("\nPrinting Test\n")
   return jsonify(result={"status":200})
