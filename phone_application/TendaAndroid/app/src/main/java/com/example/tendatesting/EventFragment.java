ce33283dd558bc2653e6f748b690741484a5438e
Hungnguyen <37910067+Hungnguyen15595@users.noreply.github.com>
2019-04-13 01:13:05 -0700

    Added Create Event Activity

--- phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
+++ phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
@@ -1,18 +1,24 @@
 package com.example.tendatesting;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.design.widget.FloatingActionButton;
 
-public class EventFragment extends Fragment {
+public class EventFragment extends Fragment implements View.OnClickListener {
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        getActivity().setTitle("Manage Event");
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_event, container, false);
     }
+
+    @Override
+    public void onClick(View v){
+
+    }
+
 }
