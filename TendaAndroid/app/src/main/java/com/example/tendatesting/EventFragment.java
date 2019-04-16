f649806cf23a63219d28cc18dbeada5262c51c77
Akshy <palanisamya@spu.edu>
2019-04-16 11:33:56 -0700

    gets long/lat but no map integration yet

--- TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
+++ TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
@@ -1,18 +1,37 @@
 package com.example.tendatesting;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class EventFragment extends Fragment {
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         getActivity().setTitle("Manage Event");
-        return inflater.inflate(R.layout.fragment_event, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_event, container, false);
+
+        Button buttonLocation = v.findViewById(R.id.button_location);
+        buttonLocation.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent myIntent = new Intent(getActivity(), LocationActivity.class);
+                startActivity(myIntent);
+            }
+        });
+        return v;
+
     }
+
+
 }
