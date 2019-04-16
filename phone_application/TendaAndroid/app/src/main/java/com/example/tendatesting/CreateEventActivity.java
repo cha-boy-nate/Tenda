d8ce02b7c246f7aceecbad3f3d1b167970372dc5
Hungnguyen <37910067+Hungnguyen15595@users.noreply.github.com>
2019-04-16 02:20:53 -0700

    Added Create Event Page, Implement Date and Time Picker for event

--- phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/CreateEventActivity.java
+++ phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/CreateEventActivity.java
@@ -1,13 +1,72 @@
 package com.example.tendatesting;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.text.format.DateFormat;
+import java.util.Calendar;
 
-public class CreateEventActivity extends AppCompatActivity {
+public class CreateEventActivity extends AppCompatActivity implements
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    Button dateAndTimePicker;
+    TextView dateTimeResult;
+
+    int day, month, year, hour,  minute;
+    int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        dateAndTimePicker = findViewById(R.id.dateAndTimePicker);
+        dateTimeResult = findViewById(R.id.dateTimeResult);
+
+        dateAndTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateEventActivity.this,
+                        CreateEventActivity.this, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        yearFinal = year;
+        monthFinal = month + 1;
+        dayFinal = dayOfMonth;
+
+        Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(CreateEventActivity.this,
+                CreateEventActivity.this, hour, minute, DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hourFinal = hourOfDay;
+        minuteFinal = minute;
+
+        dateTimeResult.setText("Date: " +monthFinal+","+dayFinal+","+yearFinal + "\n" +
+                                "Time: " +hourFinal+":"+minuteFinal );
     }
 }
