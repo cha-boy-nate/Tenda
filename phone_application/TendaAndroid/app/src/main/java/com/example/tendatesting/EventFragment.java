ba5e743be018d16c5a6dc7ff57939befc1291d20
Akshy <palanisamya@spu.edu>
2019-04-16 18:40:24 -0700

    Revert "working floateventbutton"
    This reverts commit 8186f3f77a82eea8c6355608374caea00014a155.

--- phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
+++ phone_application/TendaAndroid/app/src/main/java/com/example/tendatesting/EventFragment.java
@@ -2,36 +2,23 @@ package com.example.tendatesting;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.design.widget.FloatingActionButton;
-import android.widget.Button;
 
-public class EventFragment extends Fragment {
+public class EventFragment extends Fragment implements View.OnClickListener {
 
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getActivity().setTitle("Manage Event");
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
 
-        View v = inflater.inflate(R.layout.fragment_event, container, false);
-
-        FloatingActionButton buttonLocation = v.findViewById(R.id.createEventFab);
-        buttonLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(getActivity(), CreateEventActivity.class);
-                startActivity(myIntent);
-            }
-        });
-        return v;
+    @Override
+    public void onClick(View v){
 
     }
 
 }
-
-
