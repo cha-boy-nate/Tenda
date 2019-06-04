14e5f8867f3b685a75c0155e26f891151d4691bc
cha-boy-nate <nathanj@spu.edu>
2019-06-04 14:20:42 -0700

    updated create account everything is now operational

--- TendaAndroid/app/src/main/java/com/example/tendatesting/RegisterActivity.java
+++ TendaAndroid/app/src/main/java/com/example/tendatesting/RegisterActivity.java
@@ -35,11 +35,17 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Context context = getApplicationContext();
-                CharSequence text = "Account successfully created";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+                try {
+                    createAccount(view);
+                    CharSequence text = "Account successfully created";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -53,22 +59,11 @@ public class RegisterActivity extends AppCompatActivity {
     //Function will be called when the button is clicked.
     public void createAccount(View view) throws IOException, JSONException {
         EditText name = (EditText)findViewById(R.id.name);
-        EditText email = (EditText)findViewById(R.id.name);
+        EditText email = (EditText)findViewById(R.id.email);
         EditText password = (EditText)findViewById(R.id.password);
         final String emailString = email.getText().toString();
         final String nameString = name.getText().toString();
         final String passwordString = password.getText().toString();
-
-        final String requestString = emailString + " " + nameString + " " + passwordString;
-
-        /*
-        final String jsonString = new JSONObject()
-                .put("name", nameString
-                .put("email", emailString)
-                .put("password", passwordString).toString();
-        */
-
-        Log.d("RegisterLog", requestString);
 
         final Intent homeIntent = new Intent(this, HomeActivity.class);
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -99,16 +94,13 @@ public class RegisterActivity extends AppCompatActivity {
         }) {
             protected Map<String, String> getParams() {
                 Map<String, String> MyData = new HashMap<String, String>();
-                MyData.put("key", requestString);
+                MyData.put("email", emailString);
+                MyData.put("name", nameString);
+                MyData.put("password", passwordString);
                 return MyData;
             }
         };
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-    }
-
-    public void ClickToLoginPage(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
